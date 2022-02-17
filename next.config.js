ab4ff9407920606ef767c4e7af5d18a51199d6a6
Iram Naghmi <iramnaghmi95@gmail.com>
2022-02-17 23:41:13 +0530

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  images: {
+    domains: ['www.google.com'],
+  },
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+
